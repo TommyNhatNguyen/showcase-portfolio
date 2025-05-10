2b234fd6fac68f26d2d95e6b10e53e81e02ebfcb
nguyen anh nhat <nguyenanhnhat123456@gmail.com>
2025-05-10 21:51:54 +0700

    feat: done homepage and header navigation

--- app/components/Navigation/index.tsx
+++ app/components/Navigation/index.tsx
@@ -1,10 +1,12 @@
 import { ROUTES } from "@/app/constants/links";
 import Link from "next/link";
-import React from "react";
+import React, { useRef } from "react";
 import Button from "../Button/Button";
 import { FiArrowRight } from "react-icons/fi";
 import clsx from "clsx";
 import AnimatedTextHover from "../AnimatedTextHover";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 type Props = {
   isHamburgerActive: boolean;
@@ -12,80 +14,157 @@ type Props = {
 };
 
 const Navigation = ({ isHamburgerActive, handleHideHamburger }: Props) => {
-  const _onHideNav = () => {
+  const navRef = useRef<HTMLDivElement>(null);
+  const _onHideHamburger = () => {
     handleHideHamburger();
   };
+  useGSAP(
+    () => {
+      const tl = gsap.timeline({});
+      const bgOpacity = document.querySelector(".bg-opacity");
+      const navListItem = document.querySelectorAll(".nav__list-item");
+      if (isHamburgerActive) {
+        tl.set([navRef.current, bgOpacity], {
+          opacity: 1,
+        });
+        tl.to(bgOpacity, {
+          height: "100%",
+          duration: 0.6,
+          ease: "power3.inOut",
+        });
+        tl.to(
+          navRef.current,
+          {
+            height: "100%",
+            paddingTop: "var(--header-height)",
+            y: 0,
+            duration: 0.6,
+            ease: "power3.out",
+          },
+          "<+0.3"
+        );
+        tl.from(
+          navListItem,
+          {
+            opacity: 0,
+            x: -100,
+            stagger: 0.05,
+            duration: 0.6,
+            ease: "power3.inOut",
+          },
+          "<"
+        );
+      } else {
+        tl.to([navRef.current, bgOpacity], {
+          opacity: 0,
+        });
+        tl.set(navRef.current, {
+          height: "0",
+          y: -96,
+        });
+        tl.set(bgOpacity, {
+          height: "0",
+        });
+      }
+    },
+    {
+      scope: navRef,
+      dependencies: [isHamburgerActive],
+    }
+  );
   return (
-    <nav className={clsx("nav", isHamburgerActive && "--active")}>
-      <div className="container">
-        <ul className="nav__list">
-          <li className="nav__list-item">
-            <Link href={ROUTES.HOME} className="link">
-              <AnimatedTextHover>
-                <div className="link__text">
-                  Home <span className="link__text-position">(01)</span>
-                </div>
-              </AnimatedTextHover>
-              <Button
-                variant="icon"
-                size="md"
-                className="link__btn"
-                icon={FiArrowRight}
-                type="button"
-              />
-            </Link>
-          </li>
-          <li className="nav__list-item">
-            <Link href={ROUTES.ABOUT} className="link">
-              <AnimatedTextHover>
-                <div className="link__text">
-                  About Me <span className="link__text-position">(02)</span>
-                </div>
-              </AnimatedTextHover>
-              <Button
-                variant="icon"
-                size="md"
-                className="link__btn"
-                icon={FiArrowRight}
-                type="button"
-              />
-            </Link>
-          </li>
-          <li className="nav__list-item">
-            <Link href={ROUTES.WORK} className="link">
-              <AnimatedTextHover>
-                <div className="link__text">
-                  Works <span className="link__text-position">(03)</span>
-                </div>
-              </AnimatedTextHover>
-              <Button
-                variant="icon"
-                size="md"
-                className="link__btn"
-                icon={FiArrowRight}
-                type="button"
-              />
-            </Link>
-          </li>
-          <li className="nav__list-item">
-            <Link href={ROUTES.BLOG} className="link">
-              <AnimatedTextHover>
-                <div className="link__text">
-                  Blog <span className="link__text-position">(04)</span>
-                </div>
-              </AnimatedTextHover>
-              <Button
-                variant="icon"
-                size="md"
-                className="link__btn"
-                icon={FiArrowRight}
-                type="button"
-              />
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div className="nav-wrapper">
+      <nav
+        ref={navRef}
+        className={clsx("nav", isHamburgerActive && "--active")}
+      >
+        <div className="container">
+          <ul className="nav__list">
+            <li className="nav__list-item">
+              <Link
+                href={ROUTES.HOME}
+                className="link"
+                onClick={_onHideHamburger}
+              >
+                <AnimatedTextHover>
+                  <div className="link__text">
+                    Home <span className="link__text-position">(01)</span>
+                  </div>
+                </AnimatedTextHover>
+                <Button
+                  variant="icon"
+                  size="md"
+                  className="link__btn"
+                  icon={FiArrowRight}
+                  type="button"
+                />
+              </Link>
+            </li>
+            <li className="nav__list-item">
+              <Link
+                href={ROUTES.ABOUT}
+                className="link"
+                onClick={_onHideHamburger}
+              >
+                <AnimatedTextHover>
+                  <div className="link__text">
+                    About Me <span className="link__text-position">(02)</span>
+                  </div>
+                </AnimatedTextHover>
+                <Button
+                  variant="icon"
+                  size="md"
+                  className="link__btn"
+                  icon={FiArrowRight}
+                  type="button"
+                />
+              </Link>
+            </li>
+            <li className="nav__list-item">
+              <Link
+                href={ROUTES.WORK}
+                className="link"
+                onClick={_onHideHamburger}
+              >
+                <AnimatedTextHover>
+                  <div className="link__text">
+                    Works <span className="link__text-position">(03)</span>
+                  </div>
+                </AnimatedTextHover>
+                <Button
+                  variant="icon"
+                  size="md"
+                  className="link__btn"
+                  icon={FiArrowRight}
+                  type="button"
+                />
+              </Link>
+            </li>
+            <li className="nav__list-item">
+              <Link
+                href={ROUTES.BLOG}
+                className="link"
+                onClick={_onHideHamburger}
+              >
+                <AnimatedTextHover>
+                  <div className="link__text">
+                    Blog <span className="link__text-position">(04)</span>
+                  </div>
+                </AnimatedTextHover>
+                <Button
+                  variant="icon"
+                  size="md"
+                  className="link__btn"
+                  icon={FiArrowRight}
+                  type="button"
+                />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="bg-opacity"></div>
+    </div>
   );
 };
 
