25e913e9bc485ec2cdafdeb142768eb3e3a0da2a
nguyen anh nhat <nguyenanhnhat123456@gmail.com>
2025-05-16 20:06:14 +0700

    fix: loading

--- app/utils/checkLoading.ts
+++
@@ -1,8 +0,0 @@
-const checkLoading = () => {
-    if (!document.body.classList.contains("--loading")) {
-      tl.play();
-    } else {
-      gsap.delayedCall(0, checkLoading);
-    }
-  };
-  checkLoading();
